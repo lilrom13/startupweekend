41036e08d6935387bebada5723abdec9ef786368
Margheriti <lilrom13@gmail.com>
2016-11-19 13:10:19 -0500

    ready to dev

--- app/app.config.js
+++ app/app.config.js
@@ -13,6 +13,12 @@ var user = angular.module('myApp.user', ['ngRoute', 'myApp'])
     $routeProvider
         .when('/', {templateUrl: 'templates/view_user/user.html',
             controller: 'userCtrl'})
+        .when('/step1', {templateUrl: 'templates/view_user/step1.html',
+            controller: 'step1Ctrl'})
+        .when('/step2', {templateUrl: 'templates/view_user/step2.html',
+            controller: 'step2Ctrl'})
+        .when('/step3', {templateUrl: 'templates/view_user/step3.html',
+            controller: 'step3Ctrl'})
         .otherwise({redirectTo : '/'});
 });
 
--- app/controllers/user.js
+++ app/controllers/user.js
@@ -4,6 +4,18 @@
 
 user.controller('userCtrl', function($scope) {
     console.log("userCtrl");
-
-
 });
+
+user.controller('step1Ctrl', function($scope) {
+    console.log("step1Ctrl");
+});
+
+user.controller('step2Ctrl', function($scope) {
+    console.log("step2Ctrl");
+});
+
+user.controller('step3Ctrl', function($scope) {
+    console.log("step3Ctrl");
+});
+
+
